dd2e8c58d8a487b941bf880def4b2ea3585fb5c2
sydykbaeva.aiday@gmail.com <sydykbaeva_ai@auca.kg>
2024-01-14 22:32:39 +0600

    Fetching and displaying data to Frontend
    Summary:
    	*Create service to connect with controller on the backend
    	*Add UI, buttons to see and work with database
    Test:
    	*Test manipulating data through UI through http://localhost:3000
    and database dynamically change or we get tthe right information

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ParentModeComponent } from './parent-mode/parent-mode.component';
+import { ChildModeComponent } from './child-mode/child-mode.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AddChildService } from './add-child.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentModeComponent,
+    ChildModeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AddChildService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
