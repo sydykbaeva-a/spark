ac3c097cec71163863f4e990331a2766d8c14ced
Aidai Sydykbaeva <aidai@inkubasia.com>
2024-01-22 17:44:51 +0600

    Add habit backend functionality (Step 2)
    Summary:
    	*Update the Many to Many relationship between several entities: Child, Habit, Habit-And-Child-Map
    Test:
    	*Check the table which was displaying habits with checked children

--- server/src/habit/habit.entity.ts
+++ server/src/habit/habit.entity.ts
@@ -1,20 +1,25 @@
-import { ChildEntity } from "src/child/child.entity";
-import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ChildEntity } from 'src/child/child.entity';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('habit')
-
 export class HabitEntity {
-    @PrimaryGeneratedColumn()
-    habit_id: number;
+  @PrimaryGeneratedColumn()
+  habit_id: number;
 
-    @Column()
-    habit_name: string;
+  @Column()
+  habit_name: string;
 
-    @Column()
-    habit_child_map_id: number;
-
-    @ManyToMany(() => ChildEntity, child => child.habits)
-    @JoinTable({ name: 'habit_child_map', joinColumn: { name: 'habit_id' }, inverseJoinColumn: { name: 'child_id' } })
-    children: ChildEntity[];
-
+  @ManyToMany(() => ChildEntity, (child) => child.habitMap)
+  @JoinTable({
+    name: 'habit_child_map',
+    joinColumn: { name: 'habit_id' },
+    inverseJoinColumn: { name: 'child_id' },
+  })
+  children: ChildEntity[];
 }
--- server/src/habit/habit_child_map.entity.ts
+++ server/src/habit/habit_child_map.entity.ts
@@ -1,16 +1,31 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { ChildEntity } from 'src/child/child.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('habit_child_map')
 export class HabitChildMapEntity {
-    @PrimaryGeneratedColumn()
-    habit_child_map_id: number;
+  @PrimaryGeneratedColumn()
+  habit_child_map_id: number;
 
-    @Column()
-    child_id: number;
+  @Column()
+  child_id: number;
 
-    @Column()
-    habit_id: number;
+  @Column()
+  habit_id: number;
 
-    @Column()
-    habit_status: boolean;
+  @Column()
+  habit_status: boolean;
+
+  @ManyToOne(() => ChildEntity, (child) => child.habitMap)
+  @JoinColumn({
+    name: 'child_id',
+  })
+  children: ChildEntity[];
 }
