7b65c7756e7bcbb9e9938c97995004109f79889f
Aidai Sydykbaeva <aidai@inkubasia.com>
2024-02-02 18:47:28 +0600

    Add new qr code and edit MyCollection page

--- client/src/app/child-mode/my-collection/pop-up/pop-up.component.ts
+++ client/src/app/child-mode/my-collection/pop-up/pop-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-pop-up-component',
   templateUrl: './pop-up.component.html',
@@ -8,6 +9,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 export class PopUpComponent {
   constructor(
     public dialogRef: MatDialogRef<PopUpComponent>,
+    private router: Router,
     @Inject(MAT_DIALOG_DATA) public data: { path: string }
   ) {}
 
@@ -17,5 +19,10 @@ export class PopUpComponent {
 
   ok() {
     this.dialogRef.close();
+  }
+  spark() {
+    this.router.navigateByUrl(
+      'https://c129a0a7-9060-460e-a16d-04f8b42b3892-00-1hbtra07tb5bp.picard.replit.dev/spark.html'
+    );
   }
 }
